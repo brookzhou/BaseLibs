a9bd455f48966c2f5ed8b67665a3af1ab4c9f598
brookzhou <brook@brookzhoudemacbook-pro.local>
2019-02-26 23:22:04 +0800

    add project

---
+++ BaseLibs/BaseLibs/BaseLibs.h
@@ -0,0 +1,19 @@
+//
+//  BaseLibs.h
+//  BaseLibs
+//
+//  Created by brookzhou on 2019/2/26.
+//  Copyright © 2019 brookzhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BaseLibs.
+FOUNDATION_EXPORT double BaseLibsVersionNumber;
+
+//! Project version string for BaseLibs.
+FOUNDATION_EXPORT const unsigned char BaseLibsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BaseLibs/PublicHeader.h>
+
+
