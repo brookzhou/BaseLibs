aec091402b3bd84bec28f3aacbcbab824300372b
brookzhou <brook@brookzhoudemacbook-pro.local>
2019-03-11 01:06:26 +0800

    change

--- BaseLibs/BaseLibs/DatasConduct/DataEncryption.h
+++ BaseLibs/BaseLibs/DatasConduct/DataEncryption.h
@@ -11,7 +11,20 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface DataEncryption : NSObject
+
+/**
+ MD5 NSString加密
+
+ @param string 要加密的字符串
+ @return 返回l加密结果
+ */
 + (NSString *)md5DataEncryptionWithString:(NSString *)string;
+/**
+ MD5 NSDataData加密
+ 
+ @param data 要加密的NSData
+ @return 返回加密结果
+ */
 + (NSData *)md5DataEncryptionWithData:(NSData *)data;
 @end
 
